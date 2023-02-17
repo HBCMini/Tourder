27a72bdb395d1eb7aa4936c470338e2118cfe016
Le Tien Thinh <ltth@netcompany.com>
2023-02-17 23:51:55 +0700

    test

--- tourder/Program.cs
+++ tourder/Program.cs
@@ -11,8 +11,6 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    var a = 1;
-    a =+ 1;
 }
 
 app.UseHttpsRedirection();
