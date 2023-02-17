073e9ecefa63b73ba90d62df9fd7923707f1f390
Le Tien Thinh <ltth@netcompany.com>
2023-02-17 23:30:33 +0700

    +=

--- tourder/Program.cs
+++ tourder/Program.cs
@@ -3,14 +3,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-        var app = builder.Build();
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-app.UseExceptionHandler("/Error");
-// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-app.UseHsts();
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    var a = 1;
+    a =+ 1;
 }
 
 app.UseHttpsRedirection();
